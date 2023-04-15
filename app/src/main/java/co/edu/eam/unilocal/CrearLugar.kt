a665d9f9fbf39f4af5dcb18dcc44a1af35e51a2c
= <cardona21507@gmail.com>
2023-04-14 21:15:03 -0500

    v2

---
+++ app/src/main/java/co/edu/eam/unilocal/CrearLugar.kt
@@ -0,0 +1,17 @@
+package co.edu.eam.unilocal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CrearLugar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_lugar)
+    }
+
+ fun registrarLugar(v: View){
+
+ }
+
+}
--- app/src/main/java/co/edu/eam/unilocal/Usuario.kt
+++ app/src/main/java/co/edu/eam/unilocal/Usuario.kt
@@ -3,16 +3,10 @@ package co.edu.eam.unilocal
 import android.graphics.Bitmap
 import android.net.Uri
 
-class Usuario (nombre: String, email:String, password:String, foto: Bitmap?) {
+class Usuario (nombre: String, email:String, password:String) {
 
     var nombre: String = nombre
     var email: String = email
     var password: String = password
-    var foto: Bitmap? = foto
-
-    // ensasyo 2
-//hola perritaaaaaa
-    // farandulera
-
-
 }
+
