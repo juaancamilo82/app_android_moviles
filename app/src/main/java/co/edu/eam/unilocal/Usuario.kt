e409852d2625fdff6b0a8c9e443feccb65ac2acb
juaancamilo82 <juaancamilo82@outlook.com>
2023-04-14 19:14:49 -0600

    perrota

--- app/src/main/java/co/edu/eam/unilocal/Usuario.kt
+++ app/src/main/java/co/edu/eam/unilocal/Usuario.kt
@@ -9,5 +9,6 @@ class Usuario (nombre: String, email:String, password:String, foto: Bitmap?) {
     var email: String = email
     var password: String = password
     var foto: Bitmap? = foto
-//hola perrita
+//hola perritaaaaaa
+    // farandulera
 }
